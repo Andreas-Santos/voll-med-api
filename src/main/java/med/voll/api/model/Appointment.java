400dae9b93c53eda463c4543ab827ad1fbadb7cf
Andreas Santos <andreassantos.3003@gmail.com>
2026-03-02 20:31:18 -0300

    feat: create entity Appointment

---
+++ src/main/java/med/voll/api/model/Appointment.java
@@ -0,0 +1,25 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "appointments")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Patient patient;
+
+    private Doctor doctor;
+
+    private LocalDateTime data;
+}
