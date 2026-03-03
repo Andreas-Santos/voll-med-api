d6a6941ab159d1f336a49b9104b86ddd54d655ca
Andreas Santos <andreassantos.3003@gmail.com>
2026-03-03 08:21:10 -0300

    feat: creating globalException and ResponseError

---
+++ src/main/java/med/voll/api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package med.voll.api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ResponseError> handlerGeneralException(Exception ex) {
+        ResponseError response = new ResponseError(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
---
+++ src/main/java/med/voll/api/exception/ResponseError.java
@@ -0,0 +1,12 @@
+package med.voll.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+public record ResponseError(
+        String message,
+        HttpStatus httpStatus,
+        LocalDateTime time
+) {
+}
