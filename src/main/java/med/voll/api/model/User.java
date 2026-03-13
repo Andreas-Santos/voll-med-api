f3d85c1cd81ecda8f4f93640e569d805b48e9725
Andreas Santos <andreassantos.3003@gmail.com>
2026-03-13 11:15:42 -0300

    feat: user entity and spring security implementation

---
+++ src/main/java/med/voll/api/model/User.java
@@ -0,0 +1,26 @@
+package med.voll.api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String login;
+
+    @NotBlank
+    private String password;
+}
